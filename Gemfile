bdbef59744668df1a8541d3f89c4fbb63795d9aa
sinnkami <youremail@example.com(tomoki_16n1100488@nnn.ed.jp)>
2017-01-20 15:54:57 +0900

    ソースがサーバーを落とさなくてもリロードされるようになってて便利

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem "rack"
 gem "sinatra", "~> 2.0.0beta2", require: 'sinatra/base'
 gem "slim"
+gem "sinatra-contrib", "~>2.0.0beta2"
--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require "bundler/setup"
 Bundler.require(:default)
+require "sinatra/reloader"
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   configure do
     set :views, settings.root + "/views"
   end
